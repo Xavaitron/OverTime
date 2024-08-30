64c0dd033e9dbfb6a1ad2b7ab640e42940329d17
Pratyush Singh <ironpratyush@gmail.com>
2024-08-31 04:20:23 +0530

    Basic Intro page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from "react";
-import './App.css';  
+import React from "react";
+// import { ethers } from "ethers";
+// import { contractAbi, contractAddress } from "./Constants/constant";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Intro from "./components/intro.js";
+import "./App.css";
 
 function App() {
-  const [account, setAccount] = useState("");
-
-  const connectWallet = async () => {
-    if (window.ethereum) {
-      try {
-        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-        setAccount(accounts[0]);
-      } catch (error) {
-        console.error("Error connecting to MetaMask:", error);
-      }
-    } else {
-      alert("MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html");
-    }
-  };
-
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        setAccount(accounts[0]);
-      });
-    }
-  }, []);
-
   return (
-    <div className="app-container">
-      <h1>Connect to MetaMask</h1>
-      {account ? (
-        <div>
-          <p>Connected Wallet Address:</p>
-          <p className="wallet-address">{account}</p>
-        </div>
-      ) : (
-        <button onClick={connectWallet} className="connect-button">
-          Connect Wallet
-        </button>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Intro />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
